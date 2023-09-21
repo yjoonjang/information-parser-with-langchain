fc6368e570c86acab844fa7bbf03b828b9b94c75
yjoonjang <yjoonjang34@gmail.com>
2023-09-21 14:43:20 +0900

    feat: getting linkedin api and calling LLM with the information

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,6 +2,8 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
+from third_parties.linkedin import scrape_linkedin_profile
+
 information = """
     Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. Musk is the founder, chairman, CEO and chief technology officer of SpaceX; angel investor, CEO, product architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.; founder of the Boring Company; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is the wealthiest person in the world, with an estimated net worth of US$226 billion as of September 2023, according to the Bloomberg Billionaires Index, and $249 billion according to Forbes, primarily from his ownership stakes in both Tesla and SpaceX.[4][5][6]
 
@@ -29,4 +31,8 @@ if __name__ == "__main__":
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(information=information))
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
+    )
+
+    print(chain.run(information=linkedin_data))
