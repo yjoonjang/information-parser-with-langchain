db14788b3c9cbbbb8cdd709e9004b30f512021d7
yjoonjang <yjoonjang34@gmail.com>
2023-09-24 03:14:31 +0900

    feat: add output parser for backend process

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,21 +3,29 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
 from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.twitter import scrape_user_tweets
 
-if __name__ == "__main__":
-    print("Hello Langchain")
 
+def ice_break(name: str) -> PersonIntel:
     linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco")
 
     summary_template = """
         given the information {information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
+        3. A topic that may interest them
+        4. 2 creative Ice breakers to open a conversation with them
+        \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
@@ -26,4 +34,11 @@ if __name__ == "__main__":
 
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
-    print(chain.run(information=linkedin_data))
+    result = chain.run(information=linkedin_data)
+
+    return person_intel_parser.parse(result)
+
+
+if __name__ == "__main__":
+    print("Hello Langchain")
+    ice_break(name="Harrison Chase")
---
+++ third_parties/twitter.py
@@ -0,0 +1,41 @@
+import os
+from datetime import datetime, timezone
+import logging
+
+import tweepy
+
+logger = logging.getLogger("twitter")
+
+twitter_client = tweepy.Client(
+    bearer_token=os.environ["TWITTER_BEARER_TOKEN"],
+    consumer_key=os.environ["TWITTER_API_KEY"],
+    consumer_secret=os.environ["TWITTER_API_SECRET"],
+    access_token=os.environ["TWITTER_ACCESS_TOKEN"],
+    access_token_secret=os.environ["TWITTER_ACCESS_SECRET"],
+)
+
+
+def scrape_user_tweets(username, num_tweets=5):
+    """
+    Scrapes a Twitter user's original tweets (i.e., not retweets or replies) and returns them as a list of disctionaries.
+    Each dictionary has three fields: "time_posted" (relative to now). "text", and "url".
+    """
+    user_id = twitter_client.get_user(username=username).data.id
+    tweets = twitter_client.get_users_tweets(
+        id=user_id, max_results=num_tweets, exclude=["retweets", "replies"]
+    )
+
+    tweet_list = []
+    for tweet in tweets:
+        tweet_dict = {}
+        tweet_dict["text"] = tweet.text
+        tweet_dict[
+            "url"
+        ] = f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"
+        tweet_list.append(tweet_dict)
+
+    return tweet_list
+
+
+# if __name__ == '__main__':
+#     print(scrape_user_tweets(username='hwchase17'))
